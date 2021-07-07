3dc9eba0df7feff36ab3549d57f5ecf41b48a705
macbeth-byui <macbethc@byui.edu>
2021-07-07 13:09:26 -0600

    Added edittext and spinner to the example

--- app/src/main/java/t/macbeth/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/t/macbeth/constraintlayoutdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package t.macbeth.constraintlayoutdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // The measured height of the EditText fields is not known immediately.  The Global
+        // Layout Listener will notify us when the layout is done and then we can access
+        // the measured height.
+        ConstraintLayout layout = findViewById(R.id.layout);
+        layout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                EditText firstName = findViewById(R.id.editTextTextPersonName2);
+                EditText lastName = findViewById(R.id.editTextTextPersonName3);
+
+                // MeasuredHeight is in pixels but note that the EditText has padding around
+                // the editable space.  Therefore, reduce by 75%
+                firstName.setTextSize(TypedValue.COMPLEX_UNIT_PX, Math.round(firstName.getMeasuredHeight()*0.75));
+                lastName.setTextSize(TypedValue.COMPLEX_UNIT_PX, Math.round(lastName.getMeasuredHeight()*0.75));
+
+                // Only want to do this once so remove the listener
+                layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+
     }
+
+
 }
